d861a6817468a11088bd35fd37b03631e0a7fe86
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-05-11 13:01:03 +0530

    add config

--- server.ts
+++ server.ts
@@ -1,10 +1,8 @@
 import app from "./src/app";
+import { config } from "./src/config/config";
 const startServer = () => {
-  const port = process.env.PORT || 3000;
-  
-
-  app.listen(port, () => {
-    console.log("Listening on port : ", port);
+  app.listen(config.port, () => {
+    console.log("Listening on port : ", config.port);
   });
 };
 
