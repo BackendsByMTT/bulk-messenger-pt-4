b52c6f2c9997d1c303657d16c2746ed473635fd5
TrippyGaurav <gaurav.trippybug@gmail.com>
2024-05-11 14:04:18 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 app.use(express.json());
@@ -14,6 +15,8 @@ app.get("/", (req, res, next) => {
 
   res.json(health);
 });
+
+app.use("/api/users", userRouter);
 
 app.use(globalErrorHandler);
 
